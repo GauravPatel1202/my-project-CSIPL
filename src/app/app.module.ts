5679ad68c18746885ba53a24ecd8434115d52bd2
Gaurav Patel <gpatel@tekion.com>
2021-09-01 19:18:21 +0530

    Update cod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { TableCompComponent } from './table-comp/table-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableCompComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
