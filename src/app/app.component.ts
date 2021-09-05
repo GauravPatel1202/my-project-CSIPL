a98330d6fef983b4ace13fe3a59b31336bf0090c
Gaurav Patel <gpatel@tekion.com>
2021-09-05 18:59:00 +0530

    Update Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-project-CSIPL';
+  page = 1;
+  passenger: any; 
+  itemsPerPage = 10;
+  totalItems : any; 
+  constructor() { }
+  
+  
+  ngOnInit(): void {
+   fetch(`https://api.instantwebtools.net/v1/passenger?page=${1}&size=${this.itemsPerPage}`).then((res)=>res.json()).then((data: any) => {
+      this.passenger =  data.data;
+      this.totalItems = data.totalPages;
+    })
+  }
+  onPageChange(pageNo: number) {
+    console.log("Current page: ", pageNo);
+    fetch(`https://api.instantwebtools.net/v1/passenger?page=${pageNo}&size=${this.itemsPerPage}`).then((res)=>res.json()).then((data: any) => {
+      this.passenger =  data.data;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgxPaginationModule} from 'ngx-pagination';
-import { TableCompComponent } from './table-comp/table-comp.component';
+import { PaginationDirective } from './pagination/pagination-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableCompComponent
+    PaginationDirective
   ],
   imports: [
     BrowserModule,
